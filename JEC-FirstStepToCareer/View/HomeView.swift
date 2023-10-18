5cfb65def4bc56bf9cd63221234685a24f350d3f
znak99 <xhahekcldhkznak0113@gmail.com>
2023-10-19 00:37:10 +0900

    Add home view

---
+++ JEC-FirstStepToCareer/View/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  JEC-FirstStepToCareer
+//
+//  Created by SeungWoo Hong on 2023/10/19.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("ホームビュー")
+            .font(.custom(Font.customBold, size: 24))
+    }
+}
+
+#Preview {
+    HomeView()
+}
