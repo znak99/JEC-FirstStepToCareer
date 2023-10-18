370792516ff5637029858b7b84e31f5a1f2049e4
znak99 <xhahekcldhkznak0113@gmail.com>
2023-10-19 00:05:22 +0900

    Initial Commit

---
+++ JEC-FirstStepToCareer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JEC-FirstStepToCareer
+//
+//  Created by SeungWoo Hong on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
