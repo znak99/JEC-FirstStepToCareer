cfb288cd002181a47e3820df67dc90baca074a9a
znak99 <xhahekcldhkznak0113@gmail.com>
2023-10-19 00:43:56 +0900

    Add white app logo

--- JEC-FirstStepToCareer/View/HomeView.swift
+++ JEC-FirstStepToCareer/View/HomeView.swift
@@ -11,6 +11,10 @@ struct HomeView: View {
     var body: some View {
         Text("ホームビュー")
             .font(.custom(Font.customBold, size: 24))
+        Image("AppLogo-White")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .background(.black)
     }
 }
 
