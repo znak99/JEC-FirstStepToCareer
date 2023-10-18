b9700814b253b235a5f85956513835fd9fc04823
znak99 <xhahekcldhkznak0113@gmail.com>
2023-10-19 00:52:22 +0900

    Add home manager

---
+++ JEC-FirstStepToCareer/ViewModel/HomeManager.swift
@@ -0,0 +1,12 @@
+//
+//  HomeManager.swift
+//  JEC-FirstStepToCareer
+//
+//  Created by SeungWoo Hong on 2023/10/19.
+//
+
+import Foundation
+
+class HomeManager {
+    @Published var isAppReady = false
+}
