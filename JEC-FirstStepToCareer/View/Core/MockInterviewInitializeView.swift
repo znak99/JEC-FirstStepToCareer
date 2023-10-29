adb3f182d7195851ebdd6290c19641e489aee7ea
znak99 <xhahekcldhkznak0113@gmail.com>
2023-10-29 21:37:56 +0900

    Add permission of camera and microphone

--- JEC-FirstStepToCareer/View/Core/MockInterviewInitializeView.swift
+++ JEC-FirstStepToCareer/View/Core/MockInterviewInitializeView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct MockInterviewInitializeView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader(content: { proxy in
+            ZStack {
+                
+            }
+            .toolbar(.hidden)
+        })
     }
 }
 
