e030012ad2b67f3e59e57f34ddac92e62b93e450
znak99 <xhahekcldhkznak0113@gmail.com>
2026-04-21 21:42:37 +0900

    프로젝트 초기화

---
+++ FirstStepToCareer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstStepToCareer
+//
+//  Created by Hong Seungwoo on 2026/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstStepToCareer/FirstStepToCareerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstStepToCareerApp.swift
+//  FirstStepToCareer
+//
+//  Created by Hong Seungwoo on 2026/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstStepToCareerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
